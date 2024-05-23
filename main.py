70930b67684f5a439b96019a94bbaf0496fc180f
Catrin-baze <92102923+Catrin-baze@users.noreply.github.com>
2024-05-23 19:41:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title('多模态虚假新闻检测算法')
  
 # 设置侧边栏     Tips:所有侧边栏的元素都必须在前面加上 sidebar，不然会在主页显示
 st.sidebar.expander('')     # expander必须接受一个 label参数，我这里留了一个空白
-st.sidebar.subheader('在下方调节你的参数')       # 副标题
+st.sidebar.subheader('新闻案例选择')       # 副标题
 # st.selectbox:创造一个下拉选择框的单选题，接收参数: (题目名称， 题目选项)
-cluster_class = st.sidebar.selectbox('1.聚类数量:', list(range(2, 10)))     # 选择聚类中心，并赋值
+cluster_class = st.sidebar.selectbox('1.案例选择:', list(range(2, 5)))     # 选择聚类中心，并赋值
 minmaxscaler = st.sidebar.radio('2.是否归一化:', ['是', '否'])         # 选择是否标准化
