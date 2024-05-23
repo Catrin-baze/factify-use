646e0420e8378b13c7da7cf1c8f77db9035d6321
Catrin-baze <92102923+Catrin-baze@users.noreply.github.com>
2024-05-23 19:33:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('Uber在纽约市的搭车数据')
+st.title('多模态虚假新闻检测算法'.center(33, '-'))
+ 
+# 设置侧边栏     Tips:所有侧边栏的元素都必须在前面加上 sidebar，不然会在主页显示
+st.sidebar.expander('')     # expander必须接受一个 label参数，我这里留了一个空白
+st.sidebar.subheader('在下方调节你的参数')       # 副标题
+# st.selectbox:创造一个下拉选择框的单选题，接收参数: (题目名称， 题目选项)
+cluster_class = st.sidebar.selectbox('1.聚类数量:', list(range(2, 10)))     # 选择聚类中心，并赋值
+minmaxscaler = st.sidebar.radio('2.是否归一化:', ['是', '否'])         # 选择是否标准化
