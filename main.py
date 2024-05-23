efd54d86c315db08d24deb7a87fe2ebee9c13ba6
Catrin-baze <92102923+Catrin-baze@users.noreply.github.com>
2024-05-24 01:44:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ midpro = st.sidebar.radio('2.是否显示中间过程:', ['是', '否'])
 ifdetect = st.sidebar.button('输出检测结果')
 #image = Image.open('image/claim1.jpg')
 #st.image(image, caption='Sunrise by the mountains',use_column_width=True)
-st.subheader('新闻图像')
 if news_class == '案例一':
- image = Image.open('image/claim1.jpg')
- #st.image(image, caption='Sunrise by the mountains',use_column_width=True)
- st.image(image, caption=None, width=300, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+ st.subheader('待检测新闻')
+ image1 = Image.open('image/claim1.jpg')
+ st.image(image1, caption=None, width=300, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
  st.markdown(' **text** ：Mumbai: Actors Amitabh Bachchan, Jaya Bachchan, Abhishek Bachchan &amp; Aishwarya Rai Bachchan cast their vote at a polling booth in Juhu.')
+ st.subheader('参考证据')
+ image2 = Image.open('image/doc1.jpg')
+ st.image(image2, caption=None, width=300, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+ st.markdown(' **text** ：Seventy-two seats across nine states are voting in the fourth phase of 2019 general elections on Monday. This phase will see the end of polling in Maharashtra and Odisha, while the process will start in Rajasthan, Madhya Pradesh and Jharkhand. Twenty-six seats in Bihar, Uttar Pradesh and West Bengal are also scheduled to vote in this phase. These three states have seats going to polls in each of the seven phases in the 2019 elections.Also Read | BJP-Sena hope to repeat thumping win in MaharashtraThe Bharatiya Janata Party (BJP) has high stakes in this phase. Of the 72 seats going to polls, it won 45 in 2014. Its allies won another 11 seats. The Congress’s tally was just two. In 2009, the BJP-Congress breakup in these seats was 9-29.Follow highlights here: 64% voter turnout in Phase 4 of Lok Sabha elections 2019A voter turnout of 64 percent was recorded in the fourth phase of Lok Sabha polls on Monday, the Election Commission said.Voting was held in 72 parliamentary constituencies spread across nine states in this phase.Nearly 66 per cent turnout was recorded in the third phase of Lok Sabha polls in which voting was held in 116 seats. In the first two phases, polling was conducted in 91 and 95 seats on April 11 and April 18 respectively.Election to 543 seats across the country is being held in seven phases. The counting of votes will take place on May 23. The poll percentage in Bihar was 58.92%, 9.79% in Jammu and Kashmir, 63.77% in Jharkhand, 66.52% in Madhya Pradesh, 55.88% in Maharashtra, 64.05% in Odisha, 66.44% in Rajasthan, 55.57% in Uttar Pradesh, and 76.59% in West Bengal. ')
 
  
 
