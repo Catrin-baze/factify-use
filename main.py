800ac9538c8fda5aa43a565567617a4bd4456387
Catrin-baze <92102923+Catrin-baze@users.noreply.github.com>
2024-05-23 20:52:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
 st.title('多模态虚假新闻检测算法')
  
 # 设置侧边栏     Tips:所有侧边栏的元素都必须在前面加上 sidebar，不然会在主页显示
@@ -11,4 +12,6 @@ st.sidebar.subheader('新闻案例选择')       # 副标题
 cluster_class = st.sidebar.selectbox('1.案例选择:', list(range(1, 6)))     
 midpro = st.sidebar.radio('2.是否显示中间过程:', ['是', '否'])        
 ifdetect = st.sidebar.button('输出检测结果')
+image = Image.open('image/claim1.jpg')
+st.image(image, caption='Sunrise by the mountains',use_column_width=True)
 
